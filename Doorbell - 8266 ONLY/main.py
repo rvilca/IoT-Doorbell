c25fa6ecd44c16c9ac4c2da50f4d480a4b74da45
Renato <51806888+rvilca@users.noreply.github.com>
2021-09-08 07:57:11 -0400

    Update main.py

--- Doorbell - 8266 ONLY/main.py
+++ Doorbell - 8266 ONLY/main.py
@@ -5,16 +5,20 @@ import urequests
 
 sta = network.WLAN(network.STA_IF)
 sta.active(True)
-sta.connect("DESKTOP-DB7K25S 0198", "5894Tb=9")
+ssid = "xxxxx"
+password = "xxxxx"
+sta.connect(ssid, password)
 
 led = Pin(13, Pin.OUT)
 button = Pin(16, Pin.IN)
+
+URL = 'xxxx'
 
 while True:
     if button.value():
         print('The button has been pressed')
         led(1)
-        r = urequests.get('https://maker.ifttt.com/trigger/button_pressed/with/key/sVjPZHXL3nvngBqgrHR5c')
+        r = urequests.get(URL)
         time.sleep(2.0)
     else:
         print('Released')
